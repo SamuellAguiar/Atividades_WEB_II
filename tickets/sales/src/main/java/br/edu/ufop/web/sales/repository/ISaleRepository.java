c23ec5aff14427b10a0041a6f8c729ed09525543
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2025-12-03 12:36:15 -0300

    Atualização do ISaleRepository

--- tickets/sales/src/main/java/br/edu/ufop/web/sales/repository/ISaleRepository.java
+++ tickets/sales/src/main/java/br/edu/ufop/web/sales/repository/ISaleRepository.java
@@ -7,10 +7,8 @@ import java.util.UUID;
 
 public interface ISaleRepository extends JpaRepository<SaleEntity, UUID> {
 
-    // Método extra útil: Buscar todas as vendas de um usuário específico
     List<SaleEntity> findAllByUserId(UUID userId);
 
-    // Método extra útil: Buscar todas as vendas de um evento específico
     List<SaleEntity> findAllByEventId(UUID eventId);
 
 }
