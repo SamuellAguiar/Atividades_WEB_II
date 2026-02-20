b7dcc09c45ca4489d3a800db8c2ba3de64eefa89
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2026-02-20 17:36:03 -0300

    Adicionando the docker files

--- tickets/docker/03-create-database-notifications.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER "pg-tickets-notifications" WITH PASSWORD 'tickets-notifications-password';
-
-CREATE DATABASE "tickets-notifications";
-
-ALTER DATABASE "tickets-notifications" OWNER TO "pg-tickets-notifications";
